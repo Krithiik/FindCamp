16705178cfdcbbd6c8fe13247ed586e46f18a023
Krithiik <71966541+Krithiik@users.noreply.github.com>
2021-09-22 13:45:00 +0530

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -8,11 +8,13 @@ module.exports.register = async (req, res, next) => {
   try {
     const { email, username, password, adminCode } = req.body;
     const user = new User({ email, username });
-    if (adminCode == process.env.ADMIN_KEY) {
-      user.isAdmin = true;
-    } else {
-      req.flash("error", "Incorret admin code!!");
-      return res.redirect("/register");
+    if (adminCode) {
+      if (adminCode == process.env.ADMIN_KEY) {
+        user.isAdmin = true;
+      } else {
+        req.flash("error", "Incorret admin code!!");
+        return res.redirect("/register");
+      }
     }
     const registeredUser = await User.register(user, password);
     req.login(registeredUser, (err) => {
